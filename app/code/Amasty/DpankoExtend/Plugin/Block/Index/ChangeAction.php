d278193b6112e7751c0c64cfe19732e31cff9755
kres-dan <pankodanil@gmail.com>
2022-12-09 00:50:14 +0100

    Task-6

--- app/code/Amasty/DpankoExtend/Plugin/Block/Index/ChangeAction.php
+++ app/code/Amasty/DpankoExtend/Plugin/Block/Index/ChangeAction.php
@@ -26,15 +26,3 @@ class ChangeAction
     }
 }
 
-use Amasty\BaharOleg\Block\Form;
-
-class ChangeFormAction
-{
-    const NEW_ACTION_FORM_URL = 'checkout/cart/add';
-
-    public function afterGetFormAction(Form $subject): string
-    {
-        return $subject->getUrl(self::NEW_ACTION_FORM_URL);
-    }
-
-}
